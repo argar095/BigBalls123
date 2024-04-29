9eb2b3066885c3d796e8e1e1aa4d64b6616bdbc6
argar095 <argar05@gmail.com>
2024-04-29 10:30:17 +0700

    skibidi

--- test18.py
+++ test18.py
@@ -1,1 +1,3 @@
-print("Skibdi_Toilet")
+myfav_artists = ["banana", "annomius", "AungKhatPy"]
+myfav_artists[0] = "ah shone ma pay nal"
+print(myfav_artists)
---
+++ test20.py
@@ -0,0 +1,6 @@
+gamer_tenz = {
+    'name' : 'Sen Tenz',
+    'role' : 'duelist'
+}
+gamer_tenz["Popular_clips"] = "Ascent VCT"
+print(gamer_tenz)
